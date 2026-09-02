1abfc0ab419238a3c542512603dd86c9f0998649
vovas-coder <volodyapyrch2012@gmail.com>
2026-09-02 19:08:43 +0300

    lol

---
+++ js1.js
@@ -0,0 +1,35 @@
+let age = 14;
+console.log(age);
+
+
+let name = "vovas";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Never look back";
+console.log(myString)
+
+let myNumber = 5;
+let b = 10;
+console.log(myNumber+b)
+
+let myNull = null;
+console.log(myNull)
+
+let userName = prompt("type your name");
+
+console.log(typeof userName);
+
+alert(userName);
+
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+
+
+console.log(userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+
+confirm("Ви впевнені, що хочете продовжити?");
